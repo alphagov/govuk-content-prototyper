7936b8082149c7436228726d6e69860b67cf1598
Carlos Vilhena <carlosvilhena@gmail.com>
2017-04-21 16:31:03 +0100

    Add test controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :welcome, only: :index
+
+  root to: 'welcome#index'
 end
