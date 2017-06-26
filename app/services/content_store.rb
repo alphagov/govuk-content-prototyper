523e5a9bf958bf648599d8d8f41f502782aa04fb
Alec Gibson <aleckgibson@googlemail.com>
2017-06-26 12:17:48 +0100

    Hardcode Parenting and Childcare taxonomy
    The Parenting and Childcare taxonomy currently exists in a draft format
    on the Draft Content Store. We would like to test the navigation
    prototype with the draft taxonomy, but cannot access the draft content
    store unless we deploy the prototype to GOV.UK infrastructure.
    
    Since deploying to GOV.UK infrastructure would make the deployment
    process harder, we instead hardcode the draft taxonomy file in a JSON
    file retrieved with a Rake task from Content Tagger.
    
    When we make a request for a content item, we first check if this
    content item exists in the local JSON file. If it does, then return
    that instead.
    
    This change also updates the GOV.UK homepage to point to the correct
    taxon page, which doesn’t exist as a Production feature yet.

--- app/services/content_store.rb
+++ app/services/content_store.rb
@@ -8,11 +8,39 @@ module Services
 
   class ContentStore
     def content_item(base_path)
-      content_store_response = content_store.content_item(base_path)
+      content_store_response = local_taxons_by_base_path[base_path] || content_store.content_item(base_path)
+      add_local_taxons!(content_store_response)
       ContentItemMutator.mutate_content_item(content_store_response)
     end
 
     private
+
+    def local_taxons_by_base_path
+      @local_taxons_by_base_path ||= Config.taxons.each_with_object({}) do |taxon, hash|
+        hash[taxon['base_path']] = taxon
+      end
+    end
+
+    def local_taxons_by_content_id
+      @local_taxons_by_content_id ||= Config.taxons.each_with_object({}) do |taxon, hash|
+        hash[taxon['content_id']] = taxon
+      end
+    end
+
+    def add_local_taxons!(content_item)
+      search_result = Services.rummager.search(
+        start: 0,
+        count: RummagerSearch::PAGE_SIZE_TO_GET_EVERYTHING,
+        filter_link: content_item['base_path'],
+        fields: %w[taxons],
+      ).to_h['results'][0] || {}
+
+      taxon_ids = search_result['taxons'] || []
+      content_item['links']['taxons'] ||= [] if taxon_ids.any?
+      taxon_ids
+        .map { |taxon_id| local_taxons_by_content_id[taxon_id] }
+        .each { |taxon| content_item['links']['taxons'] << taxon if taxon }
+    end
 
     def content_store
       @content_store ||=
