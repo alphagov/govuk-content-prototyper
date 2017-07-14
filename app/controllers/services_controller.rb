44dd7e39ef9d7f21e02923bff3381a4cd0656ef9
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-07-14 13:21:17 +0100

    Define simple Page structure in JSON

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,44 +1,15 @@
 class ServicesController < ApplicationController
   def show
-    @page_schema = {
-      title: 'Register as a childminder in England',
-      description: 'You must register with Ofsted or a childminder agency if you want to be paid to look after children under 8 for more than 2 hours a day in England.',
-      blue_box_info: [
-        {title: "Register as a childminder...", url: '/url/blabla'}
-      ],
-      steps: [
-        {
-          title: 'Check if you are eligible',
-          description: 'You will need to fit certain criterea to get on the early years register.',
-          links: [
-            {
-              title: 'Check if someone can work in the UK',
-              url: '/blah',
-              description: 'hello world description'
-            },
-            {
-              title: 'Become a registered early years or childcare provider in England',
-              url: '/blah',
-              description: 'hello world description'
-            },
-            {
-              title: 'Applying to waive disqualification: early years and childcare providers',
-              url: '/blah',
-              description: 'hello world description'
-            },
-          ]
-        },
-        {
-          title: 'Contact your local council',
-          description: 'You must contact your local council before you can start applying to be a childminder.',
-          links: [
-            {
-              title: 'Find a registered childminder', url: '/blahblah'
-            }
-          ]
-        }
-      ]
-    }
+    @page_schema = ServincePageFinder.find(params[:base_path])
+  end
 
+  class ServincePageFinder
+    def self.find(base_path)
+      JSON.parse(
+        File.read("config/service_pages/#{base_path}.json")
+      ).with_indifferent_access
+    rescue Errno::ENOENT
+      return nil
+    end
   end
 end
