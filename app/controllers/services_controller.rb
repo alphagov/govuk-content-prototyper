c0273fa3e57361e017d529573ad6b5f2792c3472
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-07-17 10:28:07 +0100

    Fix typo error

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,10 +2,10 @@ class ServicesController < ApplicationController
   layout 'collections'
 
   def show
-    @page_schema = ServincePageFinder.find(params[:base_path])
+    @page_schema = ServicePageFinder.find(params[:base_path])
   end
 
-  class ServincePageFinder
+  class ServicePageFinder
     def self.find(base_path)
       file = File.read("config/service_pages/#{base_path}.json")
       JSON.parse(file, object_class: OpenStruct)
