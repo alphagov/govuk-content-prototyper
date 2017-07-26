3447f6befa75507c82ef1294322c727e1cc57fba
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-07-26 14:59:20 +0100

    Base sidebar links on the learn-to-drive-a-car json file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,7 @@
 module ApplicationHelper
-  LEARN_TO_DRIVE_BASE_PATHS = [
-    "vehicles-can-drive",
-    "driving-eyesight-rules",
-    "apply-first-provisional-driving-licence",
-    "track-your-driving-licence-application",
-    "driving-lessons-learning-to-drive/taking-driving-lessons",
-    "driving-lessons-learning-to-drive/practising-with-family-or-friends",
-    "driving-test",
-    "driving-lessons-learning-to-drive/using-l-and-p-plates",
-    "government/publications/car-show-me-tell-me-vehicle-safety-questions",
-    "complain-about-a-driving-instructor",
-    "report-an-illegal-driving-instructor",
-    "theory-test",
-    "take-practice-theory-test",
-    "book-theory-test",
-    "change-theory-test",
-    "check-theory-test",
-    "cancel-theory-test",
-    "book-driving-test",
-    "change-driving-test",
-    "check-driving-test",
-    "cancel-driving-test",
-    "find-theory-test-pass-number",
-    "apply-for-your-full-driving-licence"
-  ].freeze
-
-  def override_sidebar?(base_path)
-    LEARN_TO_DRIVE_BASE_PATHS.include?(base_path)
+  def override_sidebar?(base_path:)
+    # TODO: find a way to not hardcode the base_path
+    schema = SchemaFinderService.new(base_path: '/learn-to-drive-a-car')
+    schema.task_links.include?("/#{base_path}")
   end
 end
--- app/services/schema_finder_service.rb
+++ app/services/schema_finder_service.rb
@@ -14,13 +14,12 @@ class SchemaFinderService
 
   # keep this method for now
   # might be useful to use it for other themes other than learn to drive.
-  def override_sidebar?(base_path)
+  def task_links
     ordered_tasks = content_item.dig('links', 'ordered_steps').flat_map do |step|
       step["links"]["ordered_tasks"]
     end
 
     ordered_tasks.map! { |task| task["base_path"] }
-
-    ordered_tasks.include?(base_path)
+    ordered_tasks.select(&:present?)
   end
 end
