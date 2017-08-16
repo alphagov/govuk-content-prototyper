55cac1da79eda1c48ab05d0cdc993d5b7c4e10c6
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-08-16 11:14:30 +0100

    Tweak content item constraint
    Org pages and forms don't display correctly in our view so we need to exclude
    them from this

--- app/controllers/segment_constraints/content_item_constraint.rb
+++ app/controllers/segment_constraints/content_item_constraint.rb
@@ -4,6 +4,6 @@ class ContentItemConstraint
   end
 
   def guidance_content?(content_item)
-    content_item && content_item['government_document_supertype'] != 'forms'
+    content_item && content_item['navigation_document_supertype'] == 'guidance' && content_item['government_document_supertype'] != 'forms'
   end
 end
