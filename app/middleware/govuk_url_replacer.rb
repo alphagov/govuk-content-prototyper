53efe301103c93c39ab157ea5832e8758ec0f5ea
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-09-07 11:11:05 +0100

    Fix naked domain link
    We missed the main www.gov.uk link.  We leave the other one in to cater for all
    the links to other content.

--- app/middleware/govuk_url_replacer.rb
+++ app/middleware/govuk_url_replacer.rb
@@ -9,9 +9,10 @@ class GovukUrlReplacer
   def call(env)
     res=@app.call(env)
     base_path = env['PATH_INFO']
-
+    puts res.class
     if path_has_no_format?(base_path)
       res[2][0].gsub! "https://www.gov.uk/", "/" #res[2][0] is the response body
+      res[2][0].gsub! "https://www.gov.uk", "/" #ensure that we don't leave an empty link
       res[1]["Content-Length"] = res[2][0].length #res[1] contains the response headers
     end
     return res
