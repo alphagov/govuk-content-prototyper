5cc65e5ef4c3eb16ec92b50d4ac38867725eb502
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-02-16 16:12:09 +0000

    Use components and fix become a childminder
    This means that `/services/learn-to-drive-a-car` and `/services/become-a-childminder`
    now work, and both use the live components.
    
    Because all the GOV.UK content is currently proxied directly, no sidebars
    apart from learn to drive a car are in action.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require accordion-with-descriptions
 //= require govuk-component/tasklist
 //= require govuk-component/task-list-header
+//= require govuk_publishing_components/components/step-by-step-nav
 
 GOVUK.slidyNav.init()
 
